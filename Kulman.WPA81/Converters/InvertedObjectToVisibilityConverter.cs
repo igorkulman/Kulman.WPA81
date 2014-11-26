dbe8779e10fb2bd2ed951bcacf2ac56df720b1cf
Igor Kulman <igor@kulman.sk>
2014-11-26 12:44:36 +0100

    + new converters with tests

---
+++ Kulman.WPA81/Converters/InvertedObjectToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulman.WPA81.Converters.Abstract;
+
+namespace Kulman.WPA81.Converters
+{
+    public class InvertedObjectToVisibilityConverter: BaseVisibilityConverter<object>
+    {
+        protected override bool? ConvertToVisibility(object value)
+        {
+            if (value is string)
+            {
+                return String.IsNullOrWhiteSpace((string)value);
+            }
+
+            if (value is IEnumerable)
+            {
+                return !((IEnumerable)value).Cast<object>().Any();
+            }
+
+            return value == null;
+        }
+    }
+}
